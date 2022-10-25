899844aff994744d8d807879a86009790a3f9690
ViniciusBelt <super.vi@hotmail.com>
2022-10-25 13:02:46 -0300

    a

--- Leetcode.js
+++ Leetcode.js
@@ -23,3 +23,66 @@ A string is represented by an array if the array elements concatenated in order 
  var arrayStringsAreEqual = function(word1, word2) {
     return word1.join('') == word2.join('') ? true : false
 };
+
+// ------------------------------------------- \\
+
+/*
+You are given an array of strings arr. A string s is formed by the concatenation of a subsequence of arr that has unique characters.
+Return the maximum possible length of s.
+A subsequence is an array that can be derived from another array by deleting some or no elements without changing the order of the remaining elements.
+*/
+ var maxLength = function(arr) {
+    let maxLen = 0;
+    arr = arr.filter(isUnique);
+    const mem = {};
+    maxLen = dfs(arr, "", 0, maxLen, mem);
+    function dfs(arr, path, i, maxLen, mem) {
+        if (mem[path]) return mem[path];
+        let pathIsUnique = isUnique(path);
+        if (pathIsUnique) {
+            maxLen = Math.max(path.length, maxLen);
+        }
+        if (i === arr.length || !pathIsUnique) {
+            mem[path] = maxLen;
+            return maxLen;
+        }
+        for (let j = i; j < arr.length; j++) {
+            maxLen = dfs(arr, path + arr[j], j + 1, maxLen, mem);
+        }
+
+
+        mem[path] = maxLen;
+        return maxLen;
+    }
+
+    function isUnique(str) {
+        const set = new Set(str);
+        return set.size === str.length
+    }
+    return maxLen;
+};
+
+// ------------------------------------------- \\
+
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+*/
+var twoSum = function(nums, target) {
+    result = [];
+    index_map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        let difference = target - nums[i];
+        if (index_map.has(difference)) {
+            result[0] = i;
+            result[1] = index_map.get(difference);
+            break;
+        } else {
+            index_map.set(nums[i], i);
+        }
+    }
+    return result;
+};
+
+// ------------------------------------------- \\
