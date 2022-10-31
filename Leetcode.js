07112ae382e2cd955651a24542429e88ac088b57
ViniciusBelt <super.vi@hotmail.com>
2022-10-31 13:52:58 -0300

    .

--- Leetcode.js
+++ Leetcode.js
@@ -198,3 +198,19 @@ Return the array ans.
 var getConcatenation = function(nums) {
     return nums.concat(nums)
 };
+
+// ------------------------------------------- \\
+
+/*
+Given a zero-based permutation nums (0-indexed), build an array ans of the same length where ans[i] = nums[nums[i]] for each 0 <= i < nums.length and return it.
+A zero-based permutation nums is an array of distinct integers from 0 to nums.length - 1 (inclusive).
+*/
+
+var buildArray = function(nums) {
+    let arr = []
+    for (let i = 0; i < nums.length; i++) {
+        let position = nums [i]
+        arr.push(nums[position])
+    }
+    return arr
+};
