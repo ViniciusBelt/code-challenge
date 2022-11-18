68e4c8f4940153f547f7dd86a061f0162f7bf10e
ViniciusBelt <super.vi@hotmail.com>
2022-11-18 13:27:24 -0300

    .

--- Leetcode.js
+++ Leetcode.js
@@ -303,3 +303,46 @@ var guessNumber = function(n) {
     }
     return 1;
 };
+
+
+var addTwoNumbers = function(l1, l2) {
+    let sum = parseInt(l1.reverse().join('')) + parseInt(l2.reverse().join(''))
+    return sum.toString().split('').reverse()
+};
+
+// ------------------------------------------- \\
+
+/*
+For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+    I can be placed before V (5) and X (10) to make 4 and 9. 
+    X can be placed before L (50) and C (100) to make 40 and 90. 
+    C can be placed before D (500) and M (1000) to make 400 and 900.
+Given a roman numeral, convert it to an integer.
+*/
+
+var romanToInt = function(s) {
+    const romanMap = new Map();
+
+    romanMap.set('I', 1);
+    romanMap.set('V', 5);
+    romanMap.set('X', 10);
+    romanMap.set('L', 50);
+    romanMap.set('C', 100);
+    romanMap.set('D', 500);
+    romanMap.set('M', 1000);
+
+    const n = s.length;
+
+    let num = romanMap.get(s[n - 1]);
+
+    for (let i = n - 2; i >= 0; i--) {
+
+        if (romanMap.get(s[i]) >= romanMap.get(s[i + 1])) {
+            num += romanMap.get(s[i]);
+        } else {
+            num -= romanMap.get(s[i]);
+        }
+    }
+    return num;
+};
